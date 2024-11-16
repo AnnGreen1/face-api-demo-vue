80684373fe7a8013b5d102ba4fd7ffdf86f3f9c8
anngreen <anngreen@qq.com>
2024-11-16 12:08:31 +0800

    基本够用

---
+++ face-detector-demo/src/App.vue
@@ -0,0 +1,142 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import FaceDetector from './components/FaceDetector.vue';
+</script>
+
+<template>
+  <!-- <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <HelloWorld msg="Vite + Vue" /> -->
+  <FaceDetector />
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
+
+
+
+<style>
+:root {
+  font-family: Inter, system-ui, Avenir, Helvetica, Arial, sans-serif;
+  line-height: 1.5;
+  font-weight: 400;
+
+  color-scheme: light dark;
+  color: #242424;
+  background-color: rgba(255, 255, 255, 0.87);
+
+  font-synthesis: none;
+  text-rendering: optimizeLegibility;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+button,
+input[type="file"] {
+  border: 2px #55b881 solid;
+  border-radius: 6px;
+  background: #55b881;
+  color: white;
+}
+
+/**全局布局 */
+.layout {
+  margin: 0 auto;
+  padding: 2rem;
+  display: flex;
+  flex-direction: row;
+}
+
+.layout .layout_nav {
+  min-width: 200px;
+  border: 2px #55b881 solid;
+  border-radius: 6px;
+  padding: 10px;
+  margin-right: 20px;
+}
+
+.layout .layout_nav ul,
+.layout .layout_nav li {
+  list-style: none;
+  margin: 10px 0;
+  padding: 0;
+  line-height: 30px;
+}
+
+.layout .layout_nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+.layout .layout_nav a.router-link-exact-active {
+  color: #55b881;
+}
+
+.layout .layout_content {
+  flex: 1;
+  border: 2px #55b881 solid;
+  border-radius: 6px;
+  padding: 20px;
+}
+
+/*单页面-通用样式 */
+.page .page_option {
+  padding-bottom: 20px;
+}
+
+.page .page_option div {
+  padding: 10px;
+  border-bottom: 2px #55b881 solid;
+}
+
+.page .page_draw-target,
+.page .page_draw-discern,
+.page .page_draw {
+  position: relative;
+}
+
+.page .page_draw #page_draw-img,
+.page .page_draw #page_draw-video {
+  max-width: 720px;
+  max-height: 480px;
+}
+
+.page .page_draw #page_draw-canvas-target,
+.page .page_draw #page_draw-canvas-discern,
+.page .page_draw #page_draw-canvas {
+  position: absolute;
+  top: 0;
+  left: 0;
+}
+
+.page .page_draw #page_draw-video-canvas {
+  position: absolute;
+  top: 0;
+  left: 0;
+  max-width: 720px;
+  max-height: 480px;
+}
+
+.page .page_draw #page_draw-canvas-box {
+  border: 1px #9a9a9a solid;
+}
+</style>
+
